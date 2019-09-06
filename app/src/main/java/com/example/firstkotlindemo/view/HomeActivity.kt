6f10679705c2e23dc730a64260195143ed9e6f65
User <kartik@stalwartitsolution.co.in>
2019-09-06 18:45:50 +0530

    displayed contacts with runtime permissions in kotlin

--- app/src/main/java/com/example/firstkotlindemo/view/HomeActivity.kt
+++ app/src/main/java/com/example/firstkotlindemo/view/HomeActivity.kt
@@ -1,20 +1,33 @@
 package com.example.firstkotlindemo.view
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.util.Log
 import android.widget.ListView
-import com.example.firstkotlindemo.R
 import com.example.firstkotlindemo.adapter.CustomAdapter
 import com.example.firstkotlindemo.model.ContactModel
 import kotlinx.android.synthetic.main.activity_home.*
+import android.widget.Toast
+import android.content.pm.PackageManager
+import android.os.AsyncTask.execute
+import android.Manifest.permission
+import android.Manifest.permission.READ_CONTACTS
+import android.os.Build
+import android.database.Cursor
+import com.example.firstkotlindemo.R
+
 
 class HomeActivity : AppCompatActivity() {
 
     private var listView: ListView? = null
     private var customAdapter: CustomAdapter? = null
     private var contactModelArrayList: ArrayList<ContactModel>? = null
+    var phones: Cursor? = null
+
+    private val PERMISSIONS_REQUEST_READ_CONTACTS = 100
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +39,52 @@ class HomeActivity : AppCompatActivity() {
 
         Log.e("HomeActivity","username from MainActivity: "+strUser)
 
+        showContacts()
+
+    }
+
+    private fun showContacts() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                PERMISSIONS_REQUEST_READ_CONTACTS
+            )
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        } else {
+            // Android version is lesser than 6.0 or the permission is already granted.
+            phones = applicationContext.contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null,
+                null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+            )
+            readContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                showContacts()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Until you grant the permission, we canot display the names",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+
+    fun readContacts()
+    {
         listView = findViewById(R.id.listView) as ListView
 
         contactModelArrayList = ArrayList()
@@ -45,8 +104,7 @@ class HomeActivity : AppCompatActivity() {
 
         customAdapter = CustomAdapter(this, contactModelArrayList!!)
         listView!!.adapter = customAdapter
-
-
-
     }
+
+
 }
